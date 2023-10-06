e9b406310a7eaada2ab042408ad5adba0f7bfd51
Alexander Zhidkov <azhidkov@gmail.com>
2023-10-06 12:34:41 +0800

    add angularfire

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'screenhush',
+    appId: '1:333814295055:web:de9c39353c43851a0ad17a',
+    storageBucket: 'screenhush.appspot.com',
+    apiKey: 'AIzaSyD1nR9zpcT5tOb0kbsLNAU-yLrhZwOZCio',
+    authDomain: 'screenhush.firebaseapp.com',
+    messagingSenderId: '333814295055',
+    measurementId: 'G-VBVFV9NYDB',
+  },
+};
